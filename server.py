88d8f4eb6f85f48acf1be210f3cbff82e8bfbd72
Tfangz <tfangz@163.com>
2023-02-11 15:36:16 +0800

    Update server.py

--- server.py
+++ server.py
@@ -8,8 +8,8 @@ import logging
 import json
 
 logging.basicConfig(
-    format = "%(asctime)s %(message)s",
-    level = logging.DEBUG,
+    format="%(asctime)s %(message)s",
+    level=logging.DEBUG,
 )
 
 # msg = {
@@ -24,12 +24,14 @@ logging.basicConfig(
 
 CONNECTIONS = {}
 
+
 def parseJson(jsonMsg):
     try:
         dict = json.loads(jsonMsg)
         return dict
     except json.JSONDecodeError:
         return None
+
 
 async def send(websocket, message):
     try:
@@ -38,17 +40,19 @@ async def send(websocket, message):
         logging.error("error sending: %s", message)
         del_key = None
     for key in CONNECTIONS:
-       if websocket == CONNECTIONS[key]:
-          del_key = key
+        if websocket == CONNECTIONS[key]:
+            del_key = key
     if del_key:
         del CONNECTIONS[del_key]
-    
+
+
 def message_all(message):
     websocket.broadcast(set(CONNECTIONS.values()), message)
-    
+
+
 async def message_user(user_id, message):
     try:
-        websocket = CONNECTIONS[user_id] # raise KeyError if user disconnected
+        websocket = CONNECTIONS[user_id]  # raise KeyError if user disconnected
     except KeyError:
         logging.error("KeyError message_user user_id: %s", user_id)
         if user_id in CONNECTIONS:
@@ -60,18 +64,21 @@ async def message_user(user_id, message):
         logging.error("ConnectionClosed message_user user_id: %s", user_id)
         if user_id in CONNECTIONS:
             del CONNECTIONS[user_id]
-    
+
+
 async def forward(message):
     msg = parseJson(message)
     if "to" in msg:
         destinations = msg["to"].split(",")
         for dest in destinations:
             await message_user(dest.strip(), message)
-            
+
+
 async def broadcast(message):
     for websocket in set(CONNECTIONS.values()):
         asyncio.create_task(send(websocket, message))
-        
+
+
 async def register(name, websocket):
     if name in CONNECTIONS:
         # close connection
@@ -79,14 +86,16 @@ async def register(name, websocket):
         pass
     CONNECTIONS[name] = websocket
     logging.info("register from: %s", name)
-    
+
+
 async def bye(name, websocket):
     if name in CONNECTIONS:
-        #close connection
+        # close connection
         await websocket.close()
         del CONNECTIONS[name]
     logging.info("bye from: %s", name)
-    
+
+
 async def handler(websocket):
     print(websocket.path)
     print(websocket.remote_address[0])
@@ -95,8 +104,8 @@ async def handler(websocket):
         logging.error("error register name: %s", name)
         return
     name = name[1:]
-    await register(name, websocket) # 注册 
-    
+    await register(name, websocket)  # 注册
+
     async for message in websocket:
         try:
             msg = parseJson(message)
@@ -107,26 +116,26 @@ async def handler(websocket):
                 logging.error("format error: %s", message)
                 continue
             verb = msg["verb"]
-            if verb == 'bye': # 再见
+            if verb == "bye":  # 再见
                 await bye(name, websocket)
-            elif verb == 'broadcast':
-                await broadcast(message) # 广播
+            elif verb == "broadcast":
+                await broadcast(message)  # 广播
             else:
                 await forward(message)
         except websockets.ConnectionClosedError:
             logging.error("peer connection closed")
             websocket.close()
-            
+
+
 async def server():
     # Set the stop condition when receiving SIGTERM
     loop = asyncio.get_running_loop()
     stop = loop.create_future()
     loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-    
+
     async with websockets.serve(handler, "", 8765):
         await stop
 
+
 if __name__ == "__main__":
     asyncio.run(server())
-	
-	
